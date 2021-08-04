a24b1515d223bb2137ce4490e751f41c6c221e50
ChrisKnowsDev <chrisharley81@gmail.com>
2021-08-04 16:20:27 -0400

    styled the initial search form section

--- src/components/search-params.js
+++ src/components/search-params.js
@@ -1,25 +1,39 @@
 /** @jsxImportSource @emotion/react */
+import { useState } from 'react'
 import { css, jsx } from '@emotion/react'
 
 export default function SearchParams() {
+  const [location, setLocation] = useState('Springfield, MA')
+
   return (
     <section
       className="section"
       css={css`
-        margin-top: 8vh;
         padding: 1rem;
 
-        label,
+        form {
+          max-width: 600px;
+          margin: auto;
+        }
         input {
           display: block;
+          padding: 0.5rem;
+          margin: 0.5rem 0 1rem;
           width: 100%;
+          font-family: var(--font-family);
+          color: var(--color-mid);
         }
       `}
     >
       <form>
         <label htmlFor="location">
           Location
-          <input type="text" value="Springfield, Ma" placeholder="Location" />
+          <input
+            type="text"
+            value="Springfield, Ma"
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
         </label>
         <button className="btn btn-primary">Search</button>
       </form>
