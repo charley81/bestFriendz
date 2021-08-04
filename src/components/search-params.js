abec13a8b489db5944123997207d3f68ca6ff0ef
ChrisKnowsDev <chrisharley81@gmail.com>
2021-08-04 18:15:40 -0400

    successfully complete test data request from api

--- src/components/search-params.js
+++ src/components/search-params.js
@@ -1,9 +1,38 @@
 /** @jsxImportSource @emotion/react */
-import { useState } from 'react'
-import { css, jsx } from '@emotion/react'
+import { useState, useEffect } from 'react'
+import { css } from '@emotion/react'
+import { Client } from '@petfinder/petfinder-js'
+
+const client = new Client({
+  apiKey: 'RFFq9XTQ5PCLFzT2jHmCkZPrFwaRU9DESTRwMglscACMMA1aR7',
+  secret: 'NEJltTpb6PDOBZA2yqIz6fOcSKC8aLENWAuLZoiu',
+})
+
+const ANIMALS = ['dog', 'cat', 'bird', 'rabbit', 'reptile']
 
 export default function SearchParams() {
   const [location, setLocation] = useState('Springfield, MA')
+  const [animal, setAnimal] = useState('')
+  const [breed, setBreed] = useState('')
+  const [pets, setPets] = useState([])
+  const breeds = []
+
+  useEffect(() => {
+    requestPets()
+  })
+
+  async function requestPets() {
+    await client.animal
+      .search()
+      .then(function (response) {
+        setPets(response.data.animals)
+        console.log(pets)
+      })
+      .catch(function (error) {
+        // Handle the error
+        console.error(error)
+      })
+  }
 
   return (
     <section
@@ -15,7 +44,8 @@ export default function SearchParams() {
           max-width: 600px;
           margin: auto;
         }
-        input {
+        input,
+        select {
           display: block;
           padding: 0.5rem;
           margin: 0.5rem 0 1rem;
@@ -30,10 +60,45 @@ export default function SearchParams() {
           Location
           <input
             type="text"
-            value="Springfield, Ma"
+            id="location"
+            value={location}
             placeholder="Location"
             onChange={e => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            name="animal"
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            name="breed"
+            id="breed"
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map(breed => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button className="btn btn-primary">Search</button>
       </form>
